7cd2165e4ee6c3f8f15fa32e387258b717e9d0b7
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2024-06-07 03:05:07 +0530

    multimple comment bug fix

--- svn/trunk/html/embed.php
+++ svn/trunk/html/embed.php
@@ -14,15 +14,20 @@ $var = $GLOBALS['HYVOR_TALK_PLUGIN_JS_CONFIG'];
 
 <?php if ($var['loadMode'] === 'click'): ?>
 
+    <?php
+    $uniqueId = uniqid(); // Generate a random unique string
+
+    ?>
+
     <div style="text-align:center">
-        <button id="hyvor-talk-load-button" onclick="this.style.display='none'">
+        <button id="hyvor-talk-load-button-<?php echo $uniqueId ?>" onclick="this.style.display='none'">
             <?php echo __('Load Comments') ?>
         </button>
     </div>
 
     <script>
-        document.getElementById("hyvor-talk-load-button").addEventListener("click", function() {
-            const hyvorTalkComments = document.querySelector("hyvor-talk-comments");
+        document.getElementById("hyvor-talk-load-button-<?php echo $uniqueId ?>").addEventListener("click", function() {
+            const hyvorTalkComments = document.querySelector("hyvor-talk-comments[data-unique-id='<?php echo $uniqueId ?>']");
             hyvorTalkComments.load();
         });
     </script>
