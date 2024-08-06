3dc0f29c600db3a7bf72eb8ad4417ae30750cb0b
SupunKavinda <supunkavinda1125@gmail.com>
2024-08-06 04:41:25 +0200

    wip

--- admin/src/admin.ts
+++ admin/src/admin.ts
@@ -12,4 +12,3 @@ if (wrap) {
 
 }
 
-console.log(wrap)
--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
             input: '/src/admin.ts',
             output: {
                 entryFileNames: 'admin.js',
-            }
+            },
         }
     }
 })
