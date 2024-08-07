ae3ee85cecc5e92c9f62dafbd4178865ae83e048
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2024-08-07 16:32:01 +0530

    radio component

---
+++ admin/src/@components/Radio.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let label: string;
+    export let value: string;
+    export let radioGroup: any;
+
+    let id = label.replace(/\s+/g, '').replace(/^\w/, (c) => c.toLowerCase());
+</script>
+
+<div class="ht-radio-button">
+    <input type="radio" id={id} value={value} bind:group={radioGroup} />
+    <label for={id}>{label}</label>
+</div>
+
+<style>
+    .ht-radio-button {
+        margin-bottom: 8px;
+    }
+    label {
+        vertical-align:top;
+    }
+</style>
