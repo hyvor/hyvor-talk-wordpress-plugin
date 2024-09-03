bee122e8b979bc1d74ff13d8a63917ded45b6abb
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2024-09-03 09:51:47 +0530

    build script

--- build.sh
+++ build.sh
@@ -2,8 +2,23 @@
 
 set -e
 
-rm -rf dist
+echo "moving the plugin folder..."
+mkdir -p build/trunk/
+cp -r plugin/* build/trunk/
 
 echo "Building the project..."
-cd admin && bun run build
-echo "Build completed."
+cd admin
+rm -rf dist
+bun run build
+cp -r dist/* ../build/trunk/static/admin/
+
+echo "Copying assets..."
+cd ..
+cp -r assets build
+
+if [[ $1 == --tag=* ]]; then
+    tag="${1#--tag=}"
+    echo "Syncing the directory of the tag: $tag"
+    mkdir -p build/tags/$tag/
+    cp -r  build/trunk/* build/tags/$tag/
+fi
