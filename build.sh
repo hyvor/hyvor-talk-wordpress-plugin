9a282d6596ca8f1fdabd253a7f38f2fb0d538780
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2024-09-03 17:14:45 +0530

    workflow fix

--- build.sh
+++ build.sh
@@ -11,16 +11,19 @@ cp -r plugin/* build/trunk/
 echo "Building the project..."
 cd admin
 rm -rf dist
-bun run build
+npm run build
 cp -r dist/* ../build/trunk/static/admin/
 
 echo "Copying assets..."
 cd ..
 cp -r assets build
 
+echo "Creating the tag folder..."
 if [[ $1 == --tag=* ]]; then
     tag="${1#--tag=}"
     echo "Syncing the directory of the tag: $tag"
     mkdir -p build/tags/$tag/
     cp -r  build/trunk/* build/tags/$tag/
 fi
+
+echo "Build completed!"
